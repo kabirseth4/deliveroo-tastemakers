98af2cc7ba944f3dd6bd700fe010fec5b2b4b29b
kabirseth4 <kabirseth4@gmail.com>
2024-01-25 22:24:10 +0000

    Add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Recipient from "./components/Recipient/Recipient";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Recipient />
+      <Search />
     </div>
   );
 }
