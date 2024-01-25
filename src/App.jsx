cb6f898872c26756af68a2fd4cd75103e856946b
kabirseth4 <kabirseth4@gmail.com>
2024-01-25 21:29:32 +0000

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
