beaa2d5fbc0e5d82e29d27d8833a52f61b40acfe
Osman728 <osmanisse29@gmail.com>
2024-01-26 11:16:18 +0000

    add gift item options sturcture and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Recipient from "./components/Recipient/Recipient";
 import Search from "./components/Search/Search";
+import GiftOptions from "./components/GiftOptions/GiftOptions";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Recipient />
       <Search />
+      <GiftOptions />
     </div>
   );
 }
